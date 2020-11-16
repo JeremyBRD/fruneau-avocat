8ae32c71244d6ffa5d608fc68abac04a01ad33c1
jeremy barbedienne <jeremybarbedienne@gmail.com>
2020-11-16 18:24:59 +0100

    init react

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -32,3 +32,7 @@ document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
 });
+// Support component names relative to this directory:
+var componentRequireContext = require.context("components", true);
+var ReactRailsUJS = require("react_ujs");
+ReactRailsUJS.useContext(componentRequireContext);
