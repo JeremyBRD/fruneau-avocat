1379e5ea86a1b660a59a254071b898b902bad33c
jeremy barbedienne <jeremybarbedienne@gmail.com>
2020-12-22 00:04:05 +0100

    html and route for domaines dexpertise

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :articles
   devise_for :users
   root to: 'pages#home'
+  get 'domaines-expertise', to: 'pages#domaines_expertise'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
