f371d769578fb0388b11535dca14dd1ca616a0f5
jeremy barbedienne <jeremybarbedienne@gmail.com>
2020-12-23 00:27:32 +0100

    wip

--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -1,9 +1,9 @@
 ActionMailer::Base.smtp_settings = {
   address: ENV['POSTMARK_SMTP_SERVER'],
-  port: 587,
-  domain: 'gmail.com',
+  port: '25',
+  domain: 'heroku.com',
   user_name: ENV['POSTMARK_API_KEY'],
   password: ENV['POSTMARK_API_TOKEN'],
-  authentication: :login,
+  authentication: :cram_md5,
   enable_starttls_auto: true
 }
