183e98c2088875948d3a93c2b512caa0b2e14433
jeremy barbedienne <jeremybarbedienne@gmail.com>
2020-12-23 00:13:44 +0100

    setup smtp for mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,6 +61,9 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "fruneau_avocat_production"
 
+  config.action_mailer.delivery_method     = :smtp
+  config.action_mailer.default_url_options = { host: "fruneau-avocat.com" }
+
   config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
