28c9f1f8f9dd737c63c8ba458a5eefc99a7206ee
jeremy barbedienne <jeremybarbedienne@gmail.com>
2020-12-22 16:39:30 +0100

    add html and route for mentions legales

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   get 'domaines-expertise', to: 'pages#domaines_expertise'
   get 'cabinet', to: 'pages#cabinet'
   get 'contact', to: 'pages#contact'
+  get 'mentions-legales', to: 'pages#mentions_legales'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
