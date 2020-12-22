aa236039bdae7b8021bf0cfde9963b59dfbe5f5a
jeremy barbedienne <jeremybarbedienne@gmail.com>
2020-12-22 02:42:38 +0100

    add html, route and links for cabinet page

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   get 'domaines-expertise', to: 'pages#domaines_expertise'
+  get 'cabinet', to: 'pages#cabinet'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
