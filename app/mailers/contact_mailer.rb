194e181d0efb778d29139d866ee31d49cb543319
jeremy barbedienne <jeremybarbedienne@gmail.com>
2020-12-24 17:17:54 +0100

    fix prod

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,6 +1,7 @@
 class ContactMailer < ApplicationMailer
 
   def contact(contact)
+    @contact = contact
     mail to: "avocat@fruneau-avocat.com", subject: "Nouveau Message de fruneau-avocat.com"
   end
   
