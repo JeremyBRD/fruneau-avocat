de6ca4b00505abb2ca2835ca5d208374fbd63835
jeremy barbedienne <jeremybarbedienne@gmail.com>
2020-12-27 00:47:30 +0100

    wip

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,2 @@
 class UsersController < ApplicationController
-  # after_action :send_welcome_email
-
-  # private
-
-  # def send_welcome_email
-  #   ContactMailer.contact.deliver_now
-  # end
 end
