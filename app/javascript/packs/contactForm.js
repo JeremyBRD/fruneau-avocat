d484b8c14fac448d32f485656303fce101a18a38
jeremy barbedienne <jeremybarbedienne@gmail.com>
2020-12-26 17:19:20 +0100

    fix typo in js

--- app/javascript/packs/contactForm.js
+++ app/javascript/packs/contactForm.js
@@ -1,7 +1,6 @@
 const callToAction = document.getElementById('call-contact-form');
 const sendingBtn = document.getElementById('button-blue');
 const formDiv = document.getElementById('form-main');
-const form = document.querySelector('.form')
 
 const openContactForm = () => {
   callToAction.addEventListener('click', () => {
@@ -19,7 +18,7 @@ const closeContactForm = () => {
     if (e.path[0].id === 'form-main') {
       formDiv.style.display = 'none';
       document.body.style.position = 'unset';
-    }
+    };
   });
 }
 
