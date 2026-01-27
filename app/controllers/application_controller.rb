5aac85c560df11a0f4342d7cb172842285d7bf3b
Thibault C. <thibault.chassine@gmail.com>
2026-01-27 19:43:58 +0100

    Refactor ContactsController: extract methods for clarity

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,9 @@
 class ApplicationController < ActionController::Base
-  # before_action :authenticate_user!
   before_action :contact_info
 
   private
 
   def contact_info
-    @contact_info = ContactInfo.find_by(current_info: true)
+    @contact_info = ContactInfo.current
   end
 end
