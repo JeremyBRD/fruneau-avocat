8c76dc69510bf8c203e88aa9e0eb9db91cd7c7f5
Thibault C. <thibault.chassine@gmail.com>
2026-01-27 11:49:36 +0100

    Add route method in controller + Find current instance of contact_info

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   # before_action :authenticate_user!
+  before_action :contact_info
+
+  private
+
+  def contact_info
+    @contact_info = ContactInfo.find_by(current_info: true)
+  end
 end
