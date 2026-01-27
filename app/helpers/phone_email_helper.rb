e65cdd99ec2d01a5e37a96700d9739999f16e455
Thibault C. <thibault.chassine@gmail.com>
2026-01-27 20:16:13 +0100

    Update design mail phone link

--- app/helpers/phone_email_helper.rb
+++ app/helpers/phone_email_helper.rb
@@ -1,9 +1,9 @@
 module PhoneEmailHelper
   def phone_link
-    link_to @contact_info.phone, "tel:#{@contact_info.phone.squish}"
+    link_to @contact_info.phone, "tel:#{@contact_info.phone.squish}", class: 'link_email_phone'
   end
 
   def email_link
-    link_to @contact_info.email, "mailto:#{@contact_info.email}"
+    link_to @contact_info.email, "mailto:#{@contact_info.email}", class: 'link_email_phone'
   end
 end
